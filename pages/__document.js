c6f6611131bf693c8ea3a265c4fab073ff7bcdf3
Vladyslav Khrystych <vladyslavkhrystych@gmail.com>
2020-10-04 22:21:40 +0300

    removed google analytics, add new analytics

--- pages/__document.js
+++ pages/__document.js
@@ -9,20 +9,15 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap"
             rel="stylesheet"
           ></link>
-          >
           <script
             async
-            src="https://www.googletagmanager.com/gtag/js?id=UA-174908859-1"
+            defer
+            data-domain="vkhrystych.dev"
+            src="https://plausible.io/js/plausible.js"
           ></script>
-          <script>
-            window.dataLayer = window.dataLayer || []; function gtag()
-            {dataLayer.push(arguments)}
-            gtag('js', new Date()); gtag('config', 'UA-174908859-1');
-          </script>
         </Head>
 
         <body>
-          af
           <Main />
           <NextScript />
         </body>
