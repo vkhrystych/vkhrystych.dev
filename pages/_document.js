7a71ee77355e8eb2e4392f9401318ecd2ea6d668
Vladyslav Khrystych <vladyslavkhrystych@gmail.com>
2020-10-10 13:01:12 +0300

    rename __document.js => _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
       <Html lang="en">
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap"
             rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap"
           ></link>
+
           <script
             async
             defer
