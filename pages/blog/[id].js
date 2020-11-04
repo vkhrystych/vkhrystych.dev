80a8ad432ec39e17ce3e0b90a3f78bb165eb34d3
Vladyslav Khrystych <vladyslavkhrystych@gmail.com>
2020-11-04 20:27:39 +0200

    added a lot of content for second post

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -14,9 +14,11 @@ export default function Post({ postData }) {
 
       <article className={utilStyles.post}>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
+
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
 
         <div className={utilStyles.tags}>
